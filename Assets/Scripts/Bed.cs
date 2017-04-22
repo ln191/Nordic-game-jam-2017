174715df5d75a8519c3edb5cc91ef825d67a2dc7
Lucas <cph-ln191@cphbusiness.dk>
2017-04-22 15:43:11 +0200

    simple shuffle working

---
+++ Assets/Scripts/Bed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 currentPlacement;
+
+    private bool isMoving = false;
+
+    public Vector3 CurrentPlacement
+    {
+        get
+        {
+            return currentPlacement;
+        }
+
+        set
+        {
+            currentPlacement = value;
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
